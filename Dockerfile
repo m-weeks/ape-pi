dbe3bb028e5d23855e16585b926cce0cbfe440eb
Matthew Weeks <weekswmatt@gmail.com>
2021-09-09 21:05:09 -0600

    Fixed issues causing crashes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM wmweeks/sapi4-docker:1.2
+FROM wmweeks/sapi4-docker:1.3
 
 RUN apt install -y curl
 RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
--- controllers/bonzi.js
+++ controllers/bonzi.js
@@ -1,4 +1,5 @@
 var util = require('util');
+var fs = require('fs');
 var execFile = util.promisify(require('child_process').execFile);
 var temp = require('temp').track();
 var mkdir = util.promisify(temp.mkdir);
@@ -8,36 +9,42 @@ var mkdir = util.promisify(temp.mkdir);
  * Generates a .wav of the requested text and return it
  */
 module.exports.get = async (req, res, next) => {
+  try {
+    var directory = await mkdir('bonzi');
+    var filePath = `${directory}/bonzi.wav`;
 
-  var filePath = `${await mkdir('bonzi')}/bonzi.wav`;
+    await execFile('wine', [
+      '/app/sapi4.exe',
+      '-v',
+      'Adult Male #2, American English (TruVoice)',
+      '-s',
+      157,
+      '-p',
+      140,
+      '-t',
+      req.query.text,
+      '-o',
+      filePath
+    ]);
 
-  await execFile('wine', [
-    '/app/sapi4.exe',
-    '-v',
-    'Adult Male #2, American English (TruVoice)',
-    '-s',
-    157,
-    '-p',
-    140,
-    '-t',
-    req.query.text,
-    '-o',
-    filePath
-  ]);
+    console.log(filePath);
 
-  console.log(filePath);
+    await new Promise((resolve, reject) => {
+      res.download(filePath, (error) => {
+        if (error) {
+          reject(error);
+        }
 
-  await new Promise((resolve, reject) => {
-    res.download(filePath, (error) => {
-      if (error) {
-        reject(error);
-      }
+        resolve();
+      });
+    })
 
-      resolve();
+    fs.rm(directory, { recursive: true }, (err) => {
+      if (err) { throw err; }
     });
-  })
 
-  temp.cleanup();
-
-  next();
+    next();
+  } catch (err) {
+    next(err);
+  }
 };
